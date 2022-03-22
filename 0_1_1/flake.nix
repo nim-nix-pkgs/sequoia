fd993bb521929fcb5005c2fd139584f1582f7c61
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:19 -0300

    chore: re-index ../nimpkgs/s/sequoia

--- 0_1_1/flake.nix
+++ 0_1_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-sequoia-0_1_1.flake = false;
   inputs.src-sequoia-0_1_1.owner = "ba0f3";
-  inputs.src-sequoia-0_1_1.ref   = "refs/tags/0.1.1";
+  inputs.src-sequoia-0_1_1.ref   = "0_1_1";
   inputs.src-sequoia-0_1_1.repo  = "sequoia.nim";
   inputs.src-sequoia-0_1_1.type  = "github";
   
